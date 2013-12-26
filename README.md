8837f4596129091fe0bb21f2002edc25d1a90ba4
deathcap <deathcap@gmx.co.uk>
2013-12-25 20:07:18 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# kb-bindings-ui
+
+A graphical interface for [kb-bindings](https://github.com/deathcap/kb-bindings) using [dat-gui](https://code.google.com/p/dat-gui/).
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,28 @@
+// # vim: set shiftwidth=2 tabstop=2 softtabstop=2 expandtab:
+  
+var datgui = require('dat-gui');
+
+module.exports = function(game, opts) {
+  return new BindingsUI(game, opts);
+};
+
+function BindingsUI(game, opts) {
+  this.game = game;
+
+  opts = opts || {};
+
+  this.kb = opts.kb;
+  if (!this.kb) throw 'kb-bindings-ui requires "kb" option set to kb-bindings instance';
+  this.gui = opts.gui || new datgui.GUI();
+  this.folder = this.gui.addFolder('keys');
+
+  for (var key in this.kb.bindings) {
+    this.addKey(list[i]);
+  }
+}
+
+BindingsUI.prototype.addKey = function (name) {
+  var item = this.folder.add(this.kb.bindings, name);
+  //item.onChange(updateBinding(this, name));
+};
+
