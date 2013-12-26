a66f60ec3e47e460b6b5fd89884bafc23fe36d56
deathcap <deathcap@gmx.co.uk>
2013-12-25 20:14:25 -0800

    Iterate this.kb, skip reserved

--- index.js
+++ index.js
@@ -16,13 +16,23 @@ function BindingsUI(game, opts) {
   this.gui = opts.gui || new datgui.GUI();
   this.folder = this.gui.addFolder('keys');
 
-  for (var key in this.kb.bindings) {
-    this.addKey(list[i]);
+  for (var key in this.kb) {
+    if (isReserved(key)) continue;
+    this.addKey(key);
   }
 }
 
+function isReserved(s) {
+  return s === 'enabled' ||
+       s === 'enable' ||
+       s === 'disable' ||
+       s === 'destroy' ||
+       s === 'down' ||
+       s === 'up';
+}
+
 BindingsUI.prototype.addKey = function (name) {
-  var item = this.folder.add(this.kb.bindings, name);
+  var item = this.folder.add(this.kb, name);
   //item.onChange(updateBinding(this, name));
 };
 
